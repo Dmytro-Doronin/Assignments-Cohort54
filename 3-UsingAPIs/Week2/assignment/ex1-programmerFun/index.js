9b47ff4ca7bcc54b99f307d1eba51304a8b092c3
Dmytro-Doronin <doronindmytro623@gmail.com>
2025-10-11 19:27:12 +0200

    ex1 ex2 added

--- 3-UsingAPIs/Week2/assignment/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/assignment/ex1-programmerFun/index.js
@@ -16,29 +16,30 @@ Full description at: https://github.com/HackYourFuture/Assignments/blob/main/3-U
    url with `.shx`. There is no server at the modified url, therefore this 
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
-function requestData(url) {
-  // TODO return a promise using `fetch()`
+async function requestData(url) {
+  const response = await fetch(url);
+  return await response.json();
 }
 
 function renderImage(data) {
-  // TODO render the image to the DOM
-  console.log(data);
+  const img = document.createElement("img");
+  img.src = data.img;
+  document.body.appendChild(img);
 }
 
 function renderError(error) {
-  // TODO render the error to the DOM
-  console.log(error);
+  const h1 = document.createElement("h1");
+  h1.innerText = error;
+  document.body.appendChild(h1);
 }
 
-// TODO refactor with async/await and try/catch
-function main() {
-  requestData('https://xkcd.now.sh/?comic=latest')
-    .then((data) => {
-      renderImage(data);
-    })
-    .catch((error) => {
-      renderError(error);
-    });
+async function main() {
+    try {
+        const response = await requestData('https://xkcd.now.sh/?comic=latest');
+        renderImage(response);
+    } catch (e) {
+        renderError(e.message);
+    }
 }
 
 window.addEventListener('load', main);
--- 3-UsingAPIs/Week2/assignment/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/assignment/ex2-pokemonApp/index.js
@@ -21,18 +21,67 @@ Use async/await and try/catch to handle promises.
 Try and avoid using global variables. As much as possible, try and use function 
 parameters and return values to pass data back and forth.
 ------------------------------------------------------------------------------*/
-function fetchData(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchData(url) {
+    try {
+        const response = await fetch(url)
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+
+        return await response.json()
+    } catch (error) {
+        console.error("Fetch error:", error)
+        throw error
+    }
 }
 
-function fetchAndPopulatePokemons(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function fetchAndPopulatePokemons(data, select) {
+    select.innerHTML = ''
+    data.forEach((item) => {
+        const option = document.createElement('option')
+        option.value = item.url
+        option.textContent  = item.name
+        select.appendChild(option)
+    })
 }
 
-function fetchImage(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchImage(url) {
+   const response = await fetchData(url)
+
+    let img = document.querySelector('#pokemon-image')
+
+    if (!img) {
+        img = document.createElement('img')
+        img.id = 'pokemon-image'
+        img.alt = 'Pokemon'
+        document.body.appendChild(img)
+    }
+
+    if (response?.sprites?.front_default) {
+        img.src = response.sprites.front_default
+    } else {
+        img.src = ''
+        img.alt = 'No image available'
+    }
 }
 
-function main() {
-  // TODO complete this function
+async function main() {
+    const button = document.createElement('button')
+    button.textContent = 'Get Pokemon!'
+    document.body.appendChild(button)
+
+    const select = document.createElement('select')
+    document.body.appendChild(select)
+
+    select.addEventListener('change', async (event) => {
+        const selectedValue = event.target.value
+        await fetchImage(selectedValue)
+    })
+
+    button.addEventListener('click', async () => {
+        const data = await fetchData('https://pokeapi.co/api/v2/pokemon?limit=151')
+        fetchAndPopulatePokemons(data.results, select)
+    })
 }
+main()
